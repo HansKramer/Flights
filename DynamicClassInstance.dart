5379a498827892f76369568a90b0d78aef5d69d5
Hans Kramer <hanskramer@hermosillo.local>
2013-10-01 00:15:17 +0200

    Also import classes from libraries

--- DynamicClassInstance.dart
+++ DynamicClassInstance.dart
@@ -1,15 +1,13 @@
+/*
+       Author : Hans Kramer
+
+         Date : September 2013
+
+    Copyright : Hans Kramer
+
+ */
+
 import 'dart:mirrors';
-
-
-class Aap {
-    Aap() {
-        print("create an aap");
-    }
-
-    hello() {
-        print("Monkey!!!!");
-    }
-}
 
 
 class DynamicClassInstance {
@@ -18,20 +16,34 @@ class DynamicClassInstance {
 
     factory DynamicClassInstance(String name) {
         if (class_lut == null) {
-            var cm = currentMirrorSystem().isolate.rootLibrary.classes;
             class_lut = {};
+
+            var cm = currentMirrorSystem().isolate.rootLibrary.classes;
             cm.forEach((k,v) => class_lut[MirrorSystem.getName(k)] = v);
+
+            for (var lib in currentMirrorSystem().libraries.keys) {
+                var libname = lib.pathSegments.last.endsWith(".dart") ?
+                              lib.pathSegments.last.substring(0, lib.pathSegments.last.length-5) :
+                              lib.pathSegments.last;
+                currentMirrorSystem().libraries[lib].classes.forEach((k,v) => class_lut[libname + "." + MirrorSystem.getName(k)] = v);
+            }
         }
 
         return class_lut.containsKey(name) ? class_lut[name].newInstance(const Symbol(""), []).reflectee : null;
     }
+
+    
+    static get_classes() {
+        return class_lut.keys;
+    }
+    
+    static get_functions() {
+        return currentMirrorSystem().isolate.rootLibrary.functions.keys.map((e) => MirrorSystem.getName(e));
+    }
 }
 
-
-main() {
-    String class_name = "Aap";
-    var aap = new DynamicClassInstance(class_name);
-    aap.hello();
-    var leuk = new DynamicClassInstance("Aapje");
-    print(leuk);
+class Fuckers {
+    Fuckers() {
+       print("we are the fuckers");
+    }
 }
