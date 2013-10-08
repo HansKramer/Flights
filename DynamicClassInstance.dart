526da3df68b75dc8d0624d52f4194c3a089d7a18
Hans Kramer <hanskramer@hermosillo.local>
2013-10-08 23:15:02 +0200

    Reworked

--- DynamicClassInstance.dart
+++ DynamicClassInstance.dart
@@ -12,27 +12,33 @@ import 'dart:mirrors';
 
 class DynamicClassInstance {
 
-    static Map<String, ClassMirror> class_lut;
+    static Map<String, ClassMirror> library_class_lut;
 
-    factory DynamicClassInstance(String name) {
-        if (class_lut == null) {
-            class_lut = {};
+    factory DynamicClassInstance(String name, {String constructor: "", List arguments: null}) {
+        if (library_class_lut == null) {
+            library_class_lut = {};
 
-            var cm = currentMirrorSystem().isolate.rootLibrary.classes;
-            cm.forEach((k,v) => class_lut[MirrorSystem.getName(k)] = v);
-
-            for (var lib in currentMirrorSystem().libraries.keys) {
-                var libname = lib.pathSegments.last.endsWith(".dart") ?
-                              lib.pathSegments.last.substring(0, lib.pathSegments.last.length-5) :
-                              lib.pathSegments.last;
-                currentMirrorSystem().libraries[lib].classes.forEach((k,v) => class_lut[libname + "." + MirrorSystem.getName(k)] = v);
-            }
+            currentMirrorSystem().libraries.forEach( (uri, lib) {
+                lib.classes.forEach( (name, klass) {
+                    library_class_lut[MirrorSystem.getName(klass.qualifiedName)] = klass;
+                });
+            });
         }
 
-        return class_lut.containsKey(name) ? class_lut[name].newInstance(const Symbol(""), []).reflectee : null;
+        if (! library_class_lut.containsKey(name))
+            name = "." + name;
+        if (arguments == null)
+            arguments = [];
+        if (library_class_lut.containsKey(name)) 
+            for (var cname in library_class_lut[name].constructors.values) 
+                if (MirrorSystem.getName(cname.constructorName) == constructor) 
+                    return library_class_lut[name].newInstance(cname.constructorName, arguments).reflectee;
+
+        return null;
     }
 
     
+/*
     static get_classes() {
         return class_lut.keys;
     }
@@ -40,10 +46,5 @@ class DynamicClassInstance {
     static get_functions() {
         return currentMirrorSystem().isolate.rootLibrary.functions.keys.map((e) => MirrorSystem.getName(e));
     }
-}
-
-class Fuckers {
-    Fuckers() {
-       print("we are the fuckers");
-    }
+*/
 }
